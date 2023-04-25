9eaf1d255a27d35362799f6c3d948e4a89f28236
shrutisutrawe <77507762+shrutisutrawe@users.noreply.github.com>
2023-04-25 14:55:13 -0700

    Code changes to upload single and multi frame Dicom image, convert into PNG and display them on screen(used react-image-gallery to show multiple images)

--- server.py
+++ server.py
@@ -1,15 +1,52 @@
-from flask import Flask
+import io
+import base64
+import pydicom
+from PIL import Image
+from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+import numpy as np
 
 app = Flask(__name__)
 
+@app.route('/convert', methods=['POST'])
+def convert_dicom_to_png():
+    dicom_file = request.files['file']
+    print("dicom file:")
+    print(dicom_file)
+    filename = dicom_file.filename
+    print("filename : " + filename)
+    ds = pydicom.dcmread(dicom_file)
+    if ds.pixel_array.ndim == 2:
+        # Single frame image
+        im = Image.fromarray(ds.pixel_array)
+        im = im.convert('RGB')
+        im_data = io.BytesIO()
+        im.save(im_data, format='PNG')
+        im_data.seek(0)
+        im_base64 = base64.b64encode(im_data.read()).decode('utf-8')
+        im.show()
+        return jsonify({'success': True, 'image': im_base64})
+    elif ds.pixel_array.ndim == 3:
+        # Multi-frame image
+        print("inside multi-frame dicom image")
+        ims = []
+        for frame in ds.pixel_array:
+            im = Image.fromarray(frame)
+            im = im.convert('RGB')
+            im_data = io.BytesIO()
+            im.save(im_data, format='PNG')
+            im_data.seek(0)
+            im_base64 = base64.b64encode(im_data.read()).decode('utf-8')
+            ims.append({
+                'original': f'data:image/png;base64,{im_base64}',
+                'thumbnail': f'data:image/png;base64,{im_base64}',
+                'originalAlt': filename,
+                'thumbnailAlt': filename
+            })
+        return jsonify({'success': True,'images': ims})
+    else:
+        return jsonify({'success': False, 'error': 'Invalid DICOM image'})
 
-@app.route("/members")
-
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
-
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+if __name__ == '__main__':
     app.run(debug=True)
+
