3c9007a5985d79de8ad533f4f383b901f3b8a608
Anupama Singh <singh.anupama843@gmail.com>
2023-05-13 00:02:13 -0700

    Added alert and updated file name

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import ImageGallery from 'react-image-gallery';
 import CsvPreview from './CsvPreview'
 import JSZip from 'jszip'
+
 function App() {
   const [dicomFile, setDicomFile] = useState(null);
   const [isConversionRequest, setIsConversionRequest] = useState(false);
@@ -12,7 +13,6 @@ function App() {
   const [error, setError] = useState(null);
   const [csvMetaData, setCsvMetaData] = useState(null);
   const [isConvertInProgress, setIsConvertInProgress] = useState(false);
-
 
   const handleFileChange = (e) => {
     setDicomFile(e.target.files[0]);
@@ -23,12 +23,24 @@ function App() {
     setIsConversionRequest(false);
   };
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      alert('Please upload the file again.');
+      setTimeout(() => {
+        window.location.reload();
+      },895000); // Refresh after 5 seconds of showing the message
+    }, 900000); // Refresh every 15 minutes (15 * 60 * 1000 milliseconds)
+
+    return () => clearTimeout(timer);
+  }, []);
+
   const handleConvert = async () => {
     console.log("in convert method");
     setIsConversionRequest(true);
     const formData = new FormData();
     formData.append('file', dicomFile);
-
+    console.log(dicomFile)
+    console.log(dicomFile.name)
     setIsConvertInProgress(true)
     setError(null)
     setMultiplePngImages(null)
@@ -72,39 +84,50 @@ function App() {
     thumbnail: imageURL,
   }]
 
-  function saveAs(data, filename, type) {
+  function saveAs(data, filename) {
     var link = document.createElement("a");
-    link.setAttribute("href", type + data);
+    link.setAttribute("href", data);
     link.setAttribute("download", filename);
     
     link.style.display = "none";
       document.body.appendChild(link);
 
       // Click download link
-      link.click();
+      link.click(); 
 
       // Remove download link from DOM
       document.body.removeChild(link);
+  }
+
+  const convertFileExtension = (filename, extension) => {
+    // Save file name as the uploaded file name
+    let fileName = filename.toUpperCase()
+    console.log(fileName)
+    console.log("---->")
+    let result = fileName.replace('.DCM',`${extension}`);
+    return result
   }
 
   const handleDownload = (image_index) => {
     // Create download link
     console.log("in png download function")
     try {
-      if (image_index == 0 && singlePngImage) {
-        saveAs(singlePngImage, "image.png", '');
+      if (image_index === 0 && singlePngImage) {
+        let fileName = convertFileExtension(dicomFile.name, ".png")
+        saveAs(singlePngImage, fileName);
         return;
       }
 
       var zip = new JSZip();
       // see FileSaver.js
-
+      let fileName = convertFileExtension(dicomFile.name, "")
       multiplePngImages.map((image, index) => {
-            zip.file(image.originalAlt + index + '.png', image.original.replace('data:image/png;base64,', ""), {base64: true})
+            zip.file(fileName + "_" + index + '.png', image.original.replace('data:image/png;base64,', ""), {base64: true})
       })
 
       zip.generateAsync({type:"base64"}).then(function(content) {
-        saveAs(content, "converted_images.zip", 'data:application/zip;base64,');
+        let fileName = convertFileExtension(dicomFile.name, ".zip")
+        saveAs('data:application/zip;base64,' + content, fileName);
       });
     } catch (error){
       console.log(error)
@@ -117,18 +140,8 @@ function App() {
   const downloadMetaData = () => {
     // Create download link
     console.log("in metadata download function")
-    saveAs(encodeURIComponent(csvMetaData), "metadata.csv", 'data:text/csv;charset=utf-8,');
-  }
-
-
-  const papaparseOptions = {
-    header: true,
-    dynamicTyping: true,
-    skipEmptyLines: true,
-    transformHeader: header =>
-      header
-        .toLowerCase()
-        .replace(/\W/g, '_')
+    let fileName = convertFileExtension(dicomFile.name, ".csv")
+    saveAs('data:text/csv;charset=utf-8,' + encodeURIComponent(csvMetaData), fileName);
   }
 
   return (
@@ -137,7 +150,7 @@ function App() {
         <h1>DICOM to PNG Converter</h1>
         <div className='imageUploadSection'>
           <input type="file" onChange={handleFileChange} />
-          <button id="convertButton" onClick={handleConvert}>Convert</button>
+          <button id="convertButton" onClick={handleConvert}>Convert </button>
           {/* {error && <p>{error}</p>} */}
         </div>
       </div>
@@ -202,7 +215,6 @@ function App() {
             
           </div>
         </div>}
-
     </div>
   );
 }
