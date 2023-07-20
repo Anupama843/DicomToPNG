fdcecf2bd78bd868305b5f8f993a19c09b834aa4
Anupama Singh <singh.anupama843@gmail.com>
2023-07-20 08:48:23 -0700

    Fixed csv parsing issue

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # DicomToPNG
+
+## Setup
+### React App
+To start react server please run following commands
+```
+cd ~/DicomToPNG/client
+npm start
+```
+
+### Python Service 
+To start Python Service, please run following commands
+```
+cd ~/DicomToPNG
+source venv/bin/activate //now you sohuld see (venv) before $ in the shell that mean you env is active
+python3 server.py
+```
--- client/src/components/CsvPreview.js
+++ client/src/components/CsvPreview.js
@@ -1,31 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import '../stylesheets/csvPreview.css';
+import Papa from 'papaparse';
 
 function CsvPreview({ metadata }) {
 
     const [array, setArray] = useState([]);
+    //State to store table Column name
+    const [tableRows, setTableRows] = useState([]);
+
+    //State to store the values
+    const [values, setValues] = useState([]);
 
     useEffect(() => {
         csvFileToArray(metadata)
     }, []);
 
     const csvFileToArray = string => {
-        const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
-        const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
 
-        const array = csvRows.map(i => {
-            const values = i.split(",");
-            const obj = csvHeader.reduce((object, header, index) => {
-                object[header] = values[index];
-                return object;
-            }, {});
-            return obj;
+        Papa.parse(string, {
+            header: true,
+            skipEmptyLines: true,
+            complete: function (results) {
+                const rowsArray = [];
+                const valuesArray = [];
+
+                // Iterating data to get column name and their values
+                results.data.map((d) => {
+                    rowsArray.push(Object.keys(d));
+                    valuesArray.push(Object.values(d));
+                });
+                // Filtered Column Names
+                setTableRows(rowsArray[0]);
+
+                // Filtered Values
+                setValues(valuesArray);
+            },
         });
-
-        setArray(array);
     };
-
-    const headerKeys = Object.keys(Object.assign({}, ...array));
 
     return (
         <div style={{ textAlign: "center" }}>
@@ -34,14 +45,14 @@ function CsvPreview({ metadata }) {
                 <table>
                     <thead>
                         <tr key={"header"}>
-                            {headerKeys.map((key) => (
+                            {Object.values(tableRows).map((key) => (
                                 <th>{key}</th>
                             ))}
                         </tr>
                     </thead>
 
                     <tbody>
-                        {array.map((item) => (
+                        {values.map((item) => (
                             <tr key={item.id}>
                                 {Object.values(item).map((val) => (
                                     <td>{val}</td>
